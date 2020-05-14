cfea0d6227a9cb3009a283f9d3768e24ab5f7a0b
okabhaif <okabhaif@users.noreply.github.com>
2020-05-14 10:43:16 +0100

    Initial commit
    Simple practical to learn the core concepts of vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+  <h1>{{title}}</h1>
+
+    <input v-model="currentTodo" @submit.prevent="addTodo" @keydown.enter="addTodo()" placeholder="Add a todo">
+
+
+     <button v-on:click.prevent="addTodo">add</button>
+      <ul class="todos">
+        <li v-for="todo in todos" :key="todo.id">
+          <span v-on:click.prevent="clickToEdit(todo)" v-show="editTodoId !== todo.id" :class="{ completed: todo.completed }"> 
+            {{ todo.label }} 
+          </span> 
+
+                    <input v-model="todo.label" v-show="editTodoId == todo.id" v-on:keyup.enter="saveEdit">
+
+
+          <input type="checkbox" v-model="todo.completed">
+
+          <button v-on:click.prevent="removeTodo(todo)">remove</button> 
+        </li>
+    </ul>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+     
 
+
+<script>
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+           title: "To do list app",
+
+      todos: [],
+      currentTodo: '',
+      editTodoId: null
+    };
+  },
+  methods: {
+    addTodo() {
+      if(this.currentTodo !== ''){
+      this.todos.push({
+        id: this.todos.length, 
+        label: this.currentTodo, 
+        completed: false
+        });
+      this.currentTodo = '';
+      }
+    },
+    removeTodo(todo){
+
+var index = this.todos.indexOf(todo);
+this.todos.splice(index, 1);
+    },
+    clickToEdit (todo) {
+      this.editTodoId = todo.id; 
+    },
+    saveEdit(){
+      this.editTodoId = null;
+    }
+    
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+.completed{
+  text-decoration: line-through;
+};
 </style>
